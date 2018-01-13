21af60f6cca6879f03e9b466e1a01baf59e76dd8
joachimharms <joachimharms77@gmail.com>
2018-01-13 22:18:29 +0100

    Initial commit - static Calculator class

---
+++ Finance/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Finance
+{
+    public static class Calculator
+    {
+        #region Properties
+
+        private static decimal profitTarget;
+        
+        public static decimal ProfitTarget
+        {
+            get { return profitTarget; }
+            set { profitTarget = value; }
+        }
+
+        private static decimal lossTarget;
+
+        public static decimal LossTarget
+        {
+            get { return lossTarget; }
+            set { lossTarget = value; }
+        }
+
+        public static decimal Risk { get; set; }
+        public static decimal InitialRate { get; set; }
+        public static decimal StoppLoss { get; set; }
+        public static decimal TargetRate { get; set; }
+
+
+
+        #endregion
+
+
+        #region Methods
+
+
+        public static decimal GetChanceRiskRatio()
+        {
+            if (LossTarget != 0.00M)
+            {
+                return ProfitTarget / LossTarget;
+            }
+
+            Console.WriteLine("Es darf nicht durch Null geteilt werden!");
+            return -1;
+
+        }
+
+        public static decimal GetRisk()
+        {
+            return InitialRate - StoppLoss;
+        }
+
+        public static decimal GetTargetWinnings()
+        {
+            return TargetRate - InitialRate;
+        }
+
+        #endregion
+    }
+}
--- Finance/Program.cs
+++ Finance/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,24 @@ namespace Finance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Geben Sie das Gewinnziel ein: ");
+            Calculator.ProfitTarget = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie das Verlustziel ein: ");
+            Calculator.LossTarget = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculator.GetChanceRiskRatio());
+
+            Console.WriteLine("Geben Sie den Einstiegskurs ein: ");
+            Calculator.InitialRate = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie den Stopp - Loss Kurs ein: ");
+            Calculator.StoppLoss = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculator.GetRisk());
+
+            
+
         }
     }
 }
